d039f7bd8fa6af23b403f561afbdb03ee75d9ed6
sirkitts <sirkitts@gmail.com>
2021-04-20 07:29:10 +1000

    updated image credit

--- src/components/ImageCredit.vue
+++ src/components/ImageCredit.vue
@@ -5,6 +5,8 @@
       <a
         href="https://source.unsplash.com/"
         style="color: orange; font-size: small;"
+        target="_blank"
+        rel="noreferrer"
       >images by unsplash.com
       </a>
       &nbsp;
